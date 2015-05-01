94d12a82abccfe5865a823077323ee86e99ec23c
AizawaTakashi <aizawatakashi@AizawaTakashi-no-MacBook-Pro.local>
2015-05-01 11:28:00 +0900

    Initial Commit

---
+++ StyleLogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StyleLogs
+//
+//  Created by AizawaTakashi on 2015/05/01.
+//  Copyright (c) 2015年 AizawaTakashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
