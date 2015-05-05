014b0e5a2ff55872dde0b3e8f27602937c5199c4
AizawaTakashi <aizawatakashi@AizawaTakashi-no-MacBook-Pro.local>
2015-05-05 17:16:25 +0900

    設定画面追加

--- StyleLogs/SettingTableViewController.swift
+++ StyleLogs/SettingTableViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class SettingTableViewController: UITableViewController {
+enum KindOfInput:String {
+    case height = "身長"
+    case tartgetWeight = "目標体重"
+    case targetBodyFat = "目標体脂肪率"
+}
 
+class SettingTableViewController: UITableViewController,UITextFieldDelegate {
+
+    @IBAction func testButton(sender: AnyObject) {
+        //showInputSheet()
+    }
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var targetWeight: UITextField!
+    @IBOutlet weak var targetFat: UITextField!
+    @IBOutlet weak var version: UILabel!
+    @IBAction func heightFieldReturned(sender: AnyObject) {
+        println("")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +34,11 @@ class SettingTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        let dataManager = DataMngr.sharedInstance
+        self.targetFat.text = String(format: "%.1f", arguments: [dataManager.personalTargetBodyFat])
+        self.targetWeight.text = String(format: "%.2f", arguments: [dataManager.personalTargetWeight])
+        self.height.text = String(format: "%.1f", arguments: [dataManager.personalHeight])
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,15 +51,93 @@ class SettingTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        
+        return super.numberOfSectionsInTableView(tableView)
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return super.tableView(tableView, numberOfRowsInSection: section)
     }
 
+    func showInputSheet( kind:KindOfInput ) {
+        var inputField:UITextField!
+        let titleString:String = kind.rawValue
+        let messageString:String = kind.rawValue+"を入力してください"
+        let alertController: UIAlertController = UIAlertController(title: titleString, message: messageString, preferredStyle: .Alert)
+        let cancelAction:UIAlertAction = UIAlertAction(title: "Cancel",
+            style: UIAlertActionStyle.Cancel,
+            handler:{
+                (action:UIAlertAction!) -> Void in
+                println("Cancel")
+        })
+        let defaultAction:UIAlertAction = UIAlertAction(title: "OK",
+            style: UIAlertActionStyle.Default,
+            handler:{
+                (action:UIAlertAction!) -> Void in
+                println("OK")
+                let textFields:[UITextField] = alertController.textFields as! [UITextField]
+                let textField = textFields[0]
+                let dataManager = DataMngr.sharedInstance
+                switch kind {
+                case .height:
+                    dataManager.personalHeight =  NSString(string: textField.text).floatValue
+                    self.height.text = textField.text
+                case .targetBodyFat:
+                    let dtr = textField.text
+                    println(dtr)
+                    dataManager.personalTargetBodyFat =  NSString(string: textField.text).floatValue
+                    self.targetFat.text = textField.text
+                case .tartgetWeight:
+                    dataManager.personalTargetWeight =  NSString(string: textField.text).floatValue
+                    self.targetWeight.text = textField.text
+                default:
+                    println("error")
+                }
+                
+        })
+        alertController.addTextFieldWithConfigurationHandler({(text:UITextField!) -> Void in
+            text.keyboardType = UIKeyboardType.DecimalPad
+            //text.delegate = self
+            println("")
+        })
+        alertController.addAction(cancelAction)
+        alertController.addAction(defaultAction)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        if textField == height {
+            showInputSheet(KindOfInput.height)
+        }else if textField == targetWeight {
+            showInputSheet(KindOfInput.tartgetWeight)
+        }else{
+            showInputSheet(KindOfInput.targetBodyFat)
+        }
+        return false
+    }
+    func textFieldDidEndEditing(textField: UITextField) {
+        println("")
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let dataManager = DataMngr.sharedInstance
+        if textField == height {
+            let str = textField.text
+            let num:Float = NSString(string: str).floatValue
+            dataManager.personalHeight = num
+        }else if textField == targetWeight {
+            let str = textField.text
+            let num:Float = NSString(string: str).floatValue
+            dataManager.personalTargetWeight = num
+        }else{
+            let str = textField.text
+            let num:Float = NSString(string: str).floatValue
+            dataManager.personalTargetBodyFat = num
+        }
+        textField.resignFirstResponder()
+        return true
+    }
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
